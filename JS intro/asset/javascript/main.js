cf3989fcc728d1fd80bc6ce12de736e9d133be70
Aysel <133535989+ayselabdullayeva5@users.noreply.github.com>
2025-01-15 14:41:28 +0400

    Add files via upload

---
+++ JS intro/asset/javascript/main.js
@@ -0,0 +1,14 @@
+"use strict";
+let grade = 100;
+if(grade > 90 && grade <= 100){
+    alert("Əlaçı");
+}
+else if(grade > 70 && grade <= 90){
+    alert("Yaxşı");
+}
+else if (grade >= 50 && grade <= 70) {
+    alert("Kafi");
+}
+else{
+    alert("Kəsr");
+}
